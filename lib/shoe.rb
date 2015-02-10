b6ea1f03bfc750f0f8cb4cda37cfe49277306019
axton21 <axton_n@yahoo.com>
2015-02-10 13:49:06 -0500

    the more i learn the less i know

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+  require 'pry'
+  attr_accessor :size, :color, :material, :condition
+  attr_reader :brand
+  BRANDS = []
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
